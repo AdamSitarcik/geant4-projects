8a251086964541b913a65dea44f0af6e97e85106
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 <adamsitarcik21@gmail.com>
2022-07-08 10:52:48 +0200

    first commit

---
+++ sim1/build/CMakeFiles/sim1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim1.dir/sim1.cc.o"
+  "sim1"
+  "sim1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
