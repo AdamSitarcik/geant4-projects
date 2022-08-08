06f0aabbea7ce2dc42b41095412fc44d31468696
Adam Sitarcik <adamsitarcik21@gmail.com>
2022-08-08 12:52:46 +0200

    added proton gun to check energy deposited in STOP

--- ship_PSSD/generator.cc
+++ ship_PSSD/generator.cc
@@ -7,12 +7,16 @@ MyPrimaryGenerator::MyPrimaryGenerator()
   fAlphaParticleGun = new G4ParticleGun(1);
 
   fIonParticleGun = new G4ParticleGun(1);
+
+  fProtonParticleGun = new G4ParticleGun(1);
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
 {
   delete fElectronParticleGun;
   delete fAlphaParticleGun;
+  delete fIonParticleGun;
+  delete fProtonParticleGun;
 }
 
 void MyPrimaryGenerator::GeneratePrimaries (G4Event *anEvent)
@@ -30,6 +34,9 @@ void MyPrimaryGenerator::GeneratePrimaries (G4Event *anEvent)
   G4String particleName2 = "alpha";
   G4ParticleDefinition *alpha = particleTable->FindParticle(particleName2);
 
+  G4String particleName3 = "proton";
+  G4ParticleDefinition *proton = particleTable->FindParticle(particleName3);  
+
   G4double dirXel = G4UniformRand()-0.5;
   G4double dirYel = G4UniformRand()-0.5;
   G4double dirZel = G4UniformRand()-0.5;
@@ -44,12 +51,16 @@ void MyPrimaryGenerator::GeneratePrimaries (G4Event *anEvent)
 
   G4double ionSourcePosition = 55.*mm;
 
+  G4double protonSourcePosition = 4.*mm;
+
   G4double alphaEnergy = G4RandGauss::shoot(7155,12);
 
   G4ThreeVector posGun(0., 0., -G4RandGauss::shoot(4.3,0.1)*um);
   G4ThreeVector posIonGun(G4RandGauss::shoot(0,0.1)*mm, G4RandGauss::shoot(0,0.1)*mm, ionSourcePosition);
   G4ThreeVector dirElGun(dirXel, dirYel, dirZel);
   // G4ThreeVector dirElGun(0., 0., -1.);
+
+  G4ThreeVector posProtonGun(G4RandGauss::shoot(0,0.1)*mm, G4RandGauss::shoot(0,0.1)*mm, protonSourcePosition);
 
   G4ThreeVector dirAlGun(dirXal, dirYal, dirZal);
   // G4ThreeVector dirAlGun(0., 0., -1.);
@@ -62,26 +73,33 @@ void MyPrimaryGenerator::GeneratePrimaries (G4Event *anEvent)
   fIonParticleGun->SetParticleCharge(0.*eplus);
   fIonParticleGun->SetParticleEnergy(G4RandGauss::shoot(58,3)*MeV);
 
-  // fIonParticleGun->GeneratePrimaryVertex(anEvent);
-
   fElectronParticleGun->SetParticlePosition(posGun);
   fElectronParticleGun->SetParticleMomentumDirection(dirElGun);
   fElectronParticleGun->SetParticleDefinition(electron);
 
+  fProtonParticleGun->SetParticlePosition(posProtonGun);
+  fProtonParticleGun->SetParticleMomentumDirection(dirIonGun);
+  fProtonParticleGun->SetParticleDefinition(proton);
+  fProtonParticleGun->SetParticleEnergy(G4RandGauss::shoot(16,2)*MeV);
+
   G4double totalICC = G4RandGauss::shoot(4.28, 0.15);
   G4double ICConK = G4RandGauss::shoot(0.544, 0.05);
 
-  if(G4UniformRand()<totalICC/(totalICC+1.))
-  {
-    if(G4UniformRand()<(1/totalICC*ICConK)){
-      G4double electronEnergy = G4RandGauss::shoot(22.5,15);
-      fElectronParticleGun->SetParticleEnergy(electronEnergy*keV);
-      fElectronParticleGun->GeneratePrimaryVertex(anEvent);
-    }
-    else{
-      G4double electronEnergy = G4RandGauss::shoot(93.4,18);
-      fElectronParticleGun->SetParticleEnergy(electronEnergy*keV);
-      fElectronParticleGun->GeneratePrimaryVertex(anEvent);
+  G4int generate_electrons = 0;
+  
+  if(generate_electrons == 1){
+    if(G4UniformRand()<totalICC/(totalICC+1.))
+    {
+      if(G4UniformRand()<(1/totalICC*ICConK)){
+        G4double electronEnergy = G4RandGauss::shoot(22.5,15);
+        fElectronParticleGun->SetParticleEnergy(electronEnergy*keV);
+        fElectronParticleGun->GeneratePrimaryVertex(anEvent);
+      }
+      else{
+        G4double electronEnergy = G4RandGauss::shoot(93.4,18);
+        fElectronParticleGun->SetParticleEnergy(electronEnergy*keV);
+        fElectronParticleGun->GeneratePrimaryVertex(anEvent);
+      }
     }
   }
 
@@ -90,5 +108,7 @@ void MyPrimaryGenerator::GeneratePrimaries (G4Event *anEvent)
   fAlphaParticleGun->SetParticleMomentumDirection(dirAlGun);
   fAlphaParticleGun->SetParticleDefinition(alpha);
 
-  fAlphaParticleGun->GeneratePrimaryVertex(anEvent);
+  // fAlphaParticleGun->GeneratePrimaryVertex(anEvent);
+  // fIonParticleGun->GeneratePrimaryVertex(anEvent);
+  fProtonParticleGun->GeneratePrimaryVertex(anEvent);
 }
